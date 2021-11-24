71c72e4a4425cb21ec065444a293c8bf147c07eb
MasseElch <12862103+masseelch@users.noreply.github.com>
2021-11-24 21:02:31 +0100

    schema changes

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -13,12 +13,39 @@ type User struct {
 // Fields of the User.
 func (User) Fields() []ent.Field {
 	return []ent.Field{
-		field.Int("age"),
-		field.String("name"),
+		field.Other("other", &Link{}).
+			SchemaType(map[string]string{dialect.Postgres: "varchar"}).
+			Default(DefaultLink()),
 	}
 }
 
-// Edges of the User.
-func (User) Edges() []ent.Edge {
-	return nil
+type Link struct {
+	*url.URL
+}
+
+func DefaultLink() *Link {
+	u, _ := url.Parse("127.0.0.1")
+	return &Link{URL: u}
+}
+
+// Scan implements the Scanner interface.
+func (l *Link) Scan(value interface{}) (err error) {
+	switch v := value.(type) {
+	case nil:
+	case []byte:
+		l.URL, err = url.Parse(string(v))
+	case string:
+		l.URL, err = url.Parse(v)
+	default:
+		err = fmt.Errorf("unexpected type %T", v)
+	}
+	return
+}
+
+// Value implements the driver Valuer interface.
+func (l Link) Value() (driver.Value, error) {
+	if l.URL == nil {
+		return nil, nil
+	}
+	return l.String(), nil
 }
